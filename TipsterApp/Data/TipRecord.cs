a50d566c5d4474a8a403e997c5048c585125fb8a
Josef Procházka <josef.prochazka@bcpraha.com>
2025-05-27 11:19:35 +0200

    Initial commit

---
+++ TipsterApp/Data/TipRecord.cs
@@ -0,0 +1,12 @@
+﻿public class TipRecord
+{
+    public int TableId { get; set; }
+    public decimal BillAmount { get; set; }
+    public decimal TipAmount { get; set; }
+    public int TipPercent { get; set; }
+    public string Email { get; set; }
+    public int Rating { get; set; }
+    public DateTime Inserted { get; set; }
+    public DateTime Updated { get; set; }
+    public bool IsActive { get; set; }
+}
