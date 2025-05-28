dc4807f98f7231f5d8e82f5b411597c4074423b3
Josef Procházka <josef.prochazka@bcpraha.com>
2025-05-28 13:02:07 +0200

    Details fix

--- TipsterApp/obj/Debug/net8.0/TipsterApp.AssemblyInfo.cs
+++ TipsterApp/obj/Debug/net8.0/TipsterApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TipsterApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+117604cda7ac46018f841af35a9987ec2f632adb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0865400cf363458be165322c4e73e51752ddda5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TipsterApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TipsterApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
