6d675e74784d7a2486c6f848ed569cf6894629be
Josef Procházka <josef.prochazka@bcpraha.com>
2025-05-28 14:52:13 +0200

    Button "vlastni castka"

--- TipsterApp/obj/Debug/net8.0/TipsterApp.AssemblyInfo.cs
+++ TipsterApp/obj/Debug/net8.0/TipsterApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TipsterApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc4807f98f7231f5d8e82f5b411597c4074423b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4b55371b439549e5ed1a5e0347f8d63e22a9e72")]
 [assembly: System.Reflection.AssemblyProductAttribute("TipsterApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TipsterApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
